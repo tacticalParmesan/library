23e6f05c2bcf1ae68790c9ab70f68f488650e055
tacticalParmesan <edoardoferreri10@gmail.com>
2024-03-15 19:19:33 +0100

    Created functionality for displaying the books in cards pgrammatically, function createBookCard() in ui.js

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ const myLibrary = [];
 function Book(title, author, yearOfPublication, pages, readStatus) {
 	this.title = title;
 	this.author = author;
-	this.yearOfPublication = yearOfPublication;
+	this.year = yearOfPublication;
 	this.pages = pages;
-	this.readStatus = readStatus;
+	this.status = readStatus;
 }
 
-function updateLibrary() {
-	
-}
+
 
 function addtoLibrary(newBook) {
 	myLibrary.push(newBook);
-	updateLibrary();
 }
+
+const bookOne = new Book("Dune", "Frank Herbert", 1965, 895, false)
+const bookTwo = new Book("Crime and Punishment", "Fedor Dostoevsky", 1866, 430, false)
+const bookThree = new Book("Master and Margarita", "Mikhail Bulgakov", 1967, 372, true)
+myLibrary.push(bookOne, bookTwo, bookThree)
+console.log(myLibrary);
+
+ui.updateLibraryUI(myLibrary)
--- ui.js
+++ ui.js
@@ -1,11 +1,57 @@
 // User interface references
 const newBookButton = document.querySelector("#new-book-btn");
+const libraryDisplay = document.querySelector(".books-container");
 
 function loadDefaultEventListeners() {
 	newBookButton.addEventListener("click", () => {
-		console.log("Showing modal...")
-	})
+		alert("Showing modal...");
+	});
 }
 
 // Attaching event listeners on page load
 document.addEventListener("DOMContentLoaded", loadDefaultEventListeners);
+
+// Card creation process
+export function updateLibraryUI(libraryArray) {
+	for (let i = 0; i < libraryArray.length; i++) {
+		const bookIndex = i;
+		createBookCard(libraryArray[i], bookIndex)
+	}
+}
+
+function createBookCard(bookObject, bookIndex) {
+
+	const newBookCard = document.createElement("div");
+	newBookCard.classList.add("book-item");
+	newBookCard.setAttribute("data-index", bookIndex);
+
+	const newBookIcon = document.createElement("div");
+	newBookIcon.classList.add("book-icon")
+	newBookIcon.textContent = "📙";
+	newBookCard.appendChild(newBookIcon)
+
+	const newBookData = document.createElement("div")
+	newBookData.classList.add("book-data")
+
+	for (const key in bookObject) {
+		const bookProperty = document.createElement("div");
+		bookProperty.classList.add(`book-${String(key)}`)
+		
+		if (bookProperty.classList[0] !== "book-status") {
+			bookProperty.textContent = bookObject[key];
+		}
+
+		else {
+			const readCheckbox = document.createElement("input")
+			readCheckbox.setAttribute("type", "checkbox");
+			bookProperty.appendChild(readCheckbox)
+		}
+
+		newBookData.appendChild(bookProperty)
+
+	}
+
+	newBookCard.appendChild(newBookData);
+
+	libraryDisplay.appendChild(newBookCard)
+}
